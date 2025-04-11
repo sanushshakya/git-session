5c8cee9216faaba5dc28226a620e6d8b9b52649a
inertnp <inertnp@gmail.com>
2025-04-11 13:55:19 +0545

    prakhar le sry vabcha

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello Github")
 print("Abhishek mero ma kaam nagarana yar. afno code lekhna sika noob.")
 print("piku lai gift")
+
+
+def euta_function_ko_name():
+    return "piku sry van "
+
+print(euta_function_ko_name())
