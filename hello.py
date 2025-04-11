282055ee8adb2f19894400c4e64167f3cca6f4e4
inertnp <inertnp@gmail.com>
2025-04-11 14:00:16 +0545

    prakhar le sry vabcha

--- hello.py
+++ hello.py
@@ -7,3 +7,13 @@ def euta_function_ko_name():
     return "piku sry van "
 
 print(euta_function_ko_name())
+def abhisek_sorry():
+    print("Sorry Abhishek")
+    print("I am sorry for my behavior.")
+    print("I will not do it again.")
+    print("Please forgive me.")
+    print("I will be a good friend from now on.")
+    print("I will not let you down again.")
+    return 0
+
+abhisek_sorry()
